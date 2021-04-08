be73909d8c08be2e71a178cde12ecd6b7e07a591
Valdo <vlorian22@GMAIL.COM>
2021-04-07 22:04:10 -0500

    added constructors to Comment class an modified Comment cass

--- src/main/java/com/technews/model/Comment.java
+++ src/main/java/com/technews/model/Comment.java
@@ -6,15 +6,23 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.io.Serializable;
 
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
 @Table (name="comment")
-public class Comment {
+public class Comment implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String commentText;
     private Integer userId;
     private Integer postId;
+
+    public Comment(Integer id, String commentText, Integer userId, Integer postId) {
+        this.id = id;
+        this.commentText = commentText;
+        this.userId = userId;
+        this.postId = postId;
+    }
 }
