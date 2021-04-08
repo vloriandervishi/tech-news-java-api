ae14d4511d5ce57b837ac535cf6a4244a0e52237
Valdo <vlorian22@GMAIL.COM>
2021-04-07 22:00:17 -0500

    added class-level annotations

--- src/main/java/com/technews/model/Comment.java
+++ src/main/java/com/technews/model/Comment.java
@@ -1,5 +1,12 @@
 package com.technews.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.Entity;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
+@Table (name="comment")
 public class Comment {
     private Integer id;
     private String commentText;
