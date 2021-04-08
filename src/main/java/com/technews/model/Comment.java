a288f9bea43e8c13e62d481229c0d15862c414aa
Valdo <vlorian22@GMAIL.COM>
2021-04-07 22:01:44 -0500

    added annotations on one-instance-level variable

--- src/main/java/com/technews/model/Comment.java
+++ src/main/java/com/technews/model/Comment.java
@@ -3,11 +3,16 @@ package com.technews.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
 @Table (name="comment")
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String commentText;
     private Integer userId;
